f9f6c0cde2fbc95d569294667e9a4e0270396f50
TurbSoft <anasof.2708@gmail.com>
2024-11-02 09:49:28 -0500

    idea1
    Co-authored-by: Andres Ladino <aladinoster@users.noreply.github.com>

--- turbosoft_ceitum/src/main.cpp
+++ turbosoft_ceitum/src/main.cpp
@@ -1,18 +1,190 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//Motor 1
+const int IN1 = 12;
+const int IN2 = 11;
+const int ENA = 10;
+//Motor 2
+const int IN3 = 7;
+const int IN4 = 8;
+const int ENB = 9;
+
+//Entradas pwm valor inicial
+int pwmValueA = 0; //modificable (95)
+int pwmValueB = 0;
+
+// Sensor Definicion de todos los sensores
+// const int line_pin[5] = {2, 3, 4, 5, 6};
+
+// Si queremos usar por ejemplo solo 2
+const int S1=2; // Morado
+const int S2=3; // Azul
+const int S3=4; // Verde
+const int S4=5; // Amarillo 
+const int S5=6; // Naranja
+// Esto se puede hacer tambien asi:
+// const int line_pin[3] = {3, 4, 5};
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+   // Configuramos los pines como salida
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Configuramos los pines de entrada
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
+  
+
+  // for (int i = 0; i < 3; i++) {
+  //   pinMode(line_pin[i], INPUT);
+  // }
+
+  // Configuramos el puerto serial (via USB)
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  /*
+  1. Leer la informacion de los sensores
+  2. Enviar la informacion de los sensores al USB;
+  3. Logica de control: Como fijar los motores segun los sensores
+  4. Enviar los valores de pwm a los motores ;) 
+  */
+
+
+  // 1. Leer el valor de los sensores y ponerlo en variables
+  int inS1 = digitalRead(S1);
+  int inS2 = digitalRead(S2);
+  int inS3 = digitalRead(S3);
+  int inS4 = digitalRead(S4);
+  int inS5 = digitalRead(S5);
+
+  // 2. Escribir en el puerto serial (USB)
+  print_to_serial(inS1, inS2,inS3, inS4, inS5);
+
+  // 3. Controlar el motor 
+  compute_pwm_from_sensor(inS1, inS2, inS3, inS4, inS5);
+
+  // 4. Enviar los valores de pwm a los motores 
+  send_pwm(pwmValueA, pwmValueB);
+
+  // Mantener esto por un valor de tiempo
+  delay(100);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void print_to_serial(int inS1, int inS2, int inS3, int inS4, int inS5){
+    // Escribir lo que se recibio del sensor S2 en el USB
+    Serial.print("Pin N");
+    Serial.print(S1);
+    Serial.print(": ");
+    Serial.println(inS1 == HIGH ? "HIGH" : "LOW");
+    
+    // Escribir lo que se recibio del sensor S2 en el USB
+    Serial.print("Pin Y");
+    Serial.print(S2);
+    Serial.print(": ");
+    Serial.println(inS2 == HIGH ? "HIGH" : "LOW");
+    
+    // Escribir lo que se recibio del sensor S3 en el USB
+    Serial.print("Pin Y");
+    Serial.print(S3);
+    Serial.print(": ");
+    Serial.println(inS3 == HIGH ? "HIGH" : "LOW");
+
+    // Escribir lo que se recibio del sensor S4 en el USB
+    Serial.print("Pin Y");
+    Serial.print(S4);
+    Serial.print(": ");
+    Serial.println(inS4 == HIGH ? "HIGH" : "LOW");
+
+    // Escribir lo que se recibio del sensor S2 en el USB
+    Serial.print("Pin N");
+    Serial.print(S5);
+    Serial.print(": ");
+    Serial.println(inS5 == HIGH ? "HIGH" : "LOW");
+
+    // Las lineas de arriba se pueden implementar con el siguiente codigo:
+    // for (int i = 0; i < 3; i++) {
+    //   Serial.print("Pin ");
+    //   Serial.print(line_pin[i]);
+    //   Serial.print(": ");
+    //   Serial.println(pinState == HIGH ? "HIGH" : "LOW");
+    // }
+    
+}
+
+void send_pwm(int pwmValueA, int pwmValueB){
+
+  // Escribir los valores en el pwm
+  // Motor 1 hacia adelante
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  // Controlamos la velocidad con PWM
+  analogWrite(ENA, pwmValueA);
+
+
+  // Motor 2 también hacia adelante
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  // Controlamos la velocidad con PWM
+  analogWrite(ENB, pwmValueB);
+
+}
+
+void compute_pwm_from_sensor(int inS1, int inS2, int inS3, int inS4, int inS5){
+  // Definimos la logica del sensor 
+  /* El robot dispone de 5 entradas (sensores distribuidos asi)
+                    S3
+    S1      S2             S4       S5
+
+
+
+    MotorB                  MotorA
+    (Izquierda)              (Derecha)
+
+    Proposicion:
+    
+    Idea 1:
+
+    1. Si S3 detecta entonces pwmA = pwmB = 105 (Andar hacia adelante)
+    2. Si S2 detecta entonces pwmA = 105, pwmB=0 (Girar izquierda)
+    3. Si S4 detecta entonces pwmA = 0, pwmB=105 (Girar derecha)
+
+    Idea 2: 
+
+
+  */
+
+
+  // Sensor de la derecha
+  //
+  if (inS2 != HIGH){
+      pwmValueA = 105;
+      pwmValueB = 0;
+  } 
+  // 
+  else if (inS3 != HIGH){
+      pwmValueA = 105;
+      pwmValueB = 105;
+  }
+  //
+  else if (inS4 != HIGH){
+      pwmValueA = 0;
+      pwmValueB = 105;
+  }
+  else {
+      pwmValueA = 0;
+      pwmValueB = 0;
+  }
 }
